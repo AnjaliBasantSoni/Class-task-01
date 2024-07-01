430725ae07c7cecc241cdddf5c5c59d5a49158e7
Anjali B. Soni <132916277+AnjaliBasantSoni@users.noreply.github.com>
2024-07-01 10:56:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// 1* Create student Objects :-
+// function greet({id, name, grades}){
+//     console.log(`Hello ${name}! You are id is ${id}  and you are in ${grades} grades.`);
+// }
+//  greet({name: "john", id: 30, grades: "A"});
+
+// 2* Store Student in an Array :-
+// var arr = [01, 02, 03];
+// console.log("01");
+// console.log("03");
+// console.log("02");
+
+// 3* Create Function :-
+
+// function sum(a, b){
+//         console.log(a + b);
+//      }
+//      sum (10, 20);
+    // function sub(a, b){
+    //         console.log(a - b);
+    //      }
+    //    sub(30, 10);
+
